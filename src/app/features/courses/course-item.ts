5f1cbee04e78292bdb7f9752b21270270f92a8c1
Vitali.Rusau <Vitali_Rusau@epam.com>
2018-06-21 17:05:15 +0300

    Changed ICourse interface, added load-more-btn component

--- src/app/features/courses/course-item.ts
+++ src/app/features/courses/course-item.ts
@@ -1,7 +1,11 @@
-export interface CourseItem {
-  id: number;
-  title: string;
-  created: Date;
-  duration: number;
-  description: string;
+import { ICourse } from "./interfaces/icourse";
+
+export class CourseItem implements ICourse {
+  constructor(public id, public title, public created, public duration, public description) {
+      this.id = id;
+      this.title = title;
+      this.created = created;
+      this.duration = duration;
+      this.description = description;
+  }
 }
---
+++ src/app/features/courses/load-more-btn/load-more-btn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-load-more-btn',
+  templateUrl: './load-more-btn.component.html',
+  styleUrls: ['./load-more-btn.component.css']
+})
+export class LoadMoreBtnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
