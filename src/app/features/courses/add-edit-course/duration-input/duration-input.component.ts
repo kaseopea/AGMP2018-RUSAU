bab465d000d3f22a7417dc2bf765163493d2487c
kaseopea <kaseopea@gmail.com>
2018-07-22 14:22:23 +0300

    Task 6 Change Detection Final

--- src/app/features/courses/add-edit-course/duration-input/duration-input.component.ts
+++ src/app/features/courses/add-edit-course/duration-input/duration-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-duration-input',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./duration-input.component.css']
 })
 export class DurationInputComponent implements OnInit {
-
+  @Input() public durationInput: number;
   constructor() { }
 
   ngOnInit() {
